b07c1b8ab9383138f0f99fac80a73772f4fc6938
Aeico <eojwolf@gmail.com>
2022-03-06 16:50:20 +0100

    Compass moves, needs interp

--- src/Compass.tsx
+++ src/Compass.tsx
@@ -11,6 +11,20 @@ export const Compass: React.FC = () => {
 
   var transformVal = interpolate((frame+1)/durationInFrames, [0,1], [0.5,0.01])
 
+  var leftEnd = 870;
+  var topEnd = -450;
+
+  var leftInter = 0;
+  var topInter = 0;
+
+  if(frame>fps) {
+    leftInter = 1;
+    topInter = 1;
+  }
+
+  var leftTransformVal = interpolate(leftInter, [0,1], [0,leftEnd]);
+  var topTransformVal = interpolate(topInter, [0,1], [0,topEnd]);
+
   return (
     <AbsoluteFill
       style={{
@@ -18,6 +32,8 @@ export const Compass: React.FC = () => {
         alignItems: "center",
         padding: 0,
         paddingTop: 0,
+        left: leftTransformVal,
+        top: topTransformVal,
         transform: "scale("+transformVal+")",
       }}>
       <img src={CompassSVG}/>
